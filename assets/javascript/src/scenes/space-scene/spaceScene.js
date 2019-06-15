f3a0219519edfcf02a6a8d7feed7279846c00c60
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-15 13:40:47 -0600

    Add circle

--- assets/javascript/src/scenes/space-scene/spaceScene.js
+++ assets/javascript/src/scenes/space-scene/spaceScene.js
@@ -9,12 +9,19 @@ export class SpaceScene extends Phaser.Scene {
 	preload() {
 		const player = Constants.getSprite(Constants.keys.sprites.playerOne);
 		this.load.spritesheet(Constants.keys.sprites.playerOne, player.location, player.config);
+
+		const track = Constants.getSprite(Constants.keys.sprites.gameTrack);
+		this.load.image(Constants.keys.sprites.gameTrack, track.location);
 		// console.log(Constants);
 	}
 
 	create() {
-		this.add.sprite(200, 200, Constants.keys.sprites.playerOne);
+		const sprites = Constants.keys.sprites;
+		this.add.sprite(200, 200, sprites.playerOne);
+		this.add.image(540, 460, sprites.gameTrack);
 	}
 
-	update() {}
+	update() {
+		
+	}
 }
