f083eb32613545c6b4499349c94169be2958ef19
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-16 21:51:56 -0600

    Add attribution to sound effect

--- assets/javascript/src/const/sounds.js
+++ assets/javascript/src/const/sounds.js
@@ -6,5 +6,11 @@ export const SOUNDS = {
 		config: {
 			loop: true,
 		},
+		meta: {
+			author: 'Michael Brigida and Students',
+			license: 'CC Attribution 3.0 Unported'
+			url: 'https://opengameart.org/content/13-ambient-machine-sounds'
+			notes: 'Cut and distorted to fit in main menu track. Thank you, Michael and students!'
+		}
 	}
 };
