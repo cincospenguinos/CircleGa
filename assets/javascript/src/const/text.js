159546b54bdbde6389502c109a69486fb7080461
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-20 01:06:32 -0600

    Last quote--now go to bed

--- assets/javascript/src/const/text.js
+++ assets/javascript/src/const/text.js
@@ -39,4 +39,11 @@ export const TEXT = {
 		source: 'Peace and Violence Among 19th Century Latter-day Saints',
 		url: 'https://www.churchofjesuschrist.org/manual/gospel-topics-essays/peace-and-violence-among-19th-century-latter-day-saints?lang=eng',
 	},
+	circlevilleMassacre: {
+		type: 'quote',
+		text: 'In April 1866 the white settlers of Circleville annihilated a band of captive Paiute Indians, including helpless women and children. This incident of the Black Hawk War of 1865-68 was the largest massacre of Indians in Utah's history.',
+		author: 'Albert Winkler',
+		source: "The Circleville Massacre: A Brutal Incident in Utah's Black Hawk War",
+		url: 'https://scholarsarchive.byu.edu/cgi/viewcontent.cgi?article=2868&context=facpub',
+	}
 }
