2b2141f8fcbdcff777eca1dcd1841411fefb6aa1
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-22 15:06:16 -0600

    No seriously--this is cool, but it needs a lot of work

--- assets/javascript/src/scenes/level-editor/levelEditorScene.js
+++ assets/javascript/src/scenes/level-editor/levelEditorScene.js
@@ -39,12 +39,14 @@ export class LevelEditorScene extends Phaser.Scene {
         duration: 2000,
         yoyo: false,
         repeat: -1,
-        ease: "Sine.easeInOut",
+        ease: "Linear",
         callbackScope: this,
         onUpdate: function(tween, target) {
           const position = this.bezier.getTweenPoint(target.val);
+          const angle = Phaser.Math.Angle.Between(this.movingPoint.x, this.movingPoint.y, position.x, position.y) + Math.PI / 2;
           this.movingPoint.x = position.x;
           this.movingPoint.y = position.y;
+          this.movingPoint.rotation = angle;
         }
     });
 	}
--- assets/javascript/src/scenes/level-editor/model/bezier.js
+++ assets/javascript/src/scenes/level-editor/model/bezier.js
@@ -7,23 +7,24 @@ export class Bezier {
 	constructor(input, graphics, opts = {}) {
 		this.points = opts.points;
 		this.graphics = graphics;
-		input.setDraggable(this.points);
-		input.on('drag', (_, point, posX, posY) => {
+		this.input = input;
+		this.input.setDraggable(this.points);
+		this.input.on('drag', (_, point, posX, posY) => {
 			point.x = posX;
 			point.y = posY;
 			this.draw();
 		});
-		this.bezier = new Phaser.Curves.CubicBezier(this.points[0], this.points[1], this.points[2], this.points[3]);
+		this.bezierCurve = new Phaser.Curves.CubicBezier(this.points[0], this.points[1], this.points[2], this.points[3]);
 	}
 
 	getTweenPoint(val) {
-		return this.bezier.getPoint(val);
+		return this.bezierCurve.getPoint(val);
 	}
 
 	draw() {
 		this.graphics.clear();
     this.graphics.lineStyle(4, 0xffffff);
-    this.bezier.draw(this.graphics);
+    this.bezierCurve.draw(this.graphics);
     this.graphics.lineStyle(2, 0x00ff00);
     this.graphics.beginPath();
     this.graphics.moveTo(this.points[0].x, this.points[0].y);
@@ -34,5 +35,9 @@ export class Bezier {
     this.graphics.moveTo(this.points[2].x, this.points[2].y);
     this.graphics.lineTo(this.points[3].x, this.points[3].y);
     this.graphics.strokePath();
+	}
+
+	setPoints(points) {
+
 	}
 }
