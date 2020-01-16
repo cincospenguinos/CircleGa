67ba3fb1749d186cb8601302b4a5313e86bc6d6b
Andre LaFleur <andre.lafleur@utah.edu>
2020-01-16 11:13:42 -0700

    Set things up to work with babel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import Phaser from 'phaser';
 import { Constants } from './const/index.js';
 import { MenuScene } from './scenes/menu/menuScene.js';
-// import { SpaceScene } from './scenes/space-scene/spaceScene.js';
+import { SpaceScene } from './scenes/space-scene/spaceScene.js';
 // import { LevelEditorScene } from './scenes/level-editor/levelEditorScene.js';
 // import { TextScene } from './scenes/text-scene/textScene.js';
 // import { CommunicationScene } from './scenes/communication-scene/communicationScene.js';
 // import { EndScene } from './scenes/end-scene/endScene.js';
 
 const screenDimensions = Constants.dimensions.screen;
-const scenes = [ MenuScene, /* SpaceScene, TextScene, CommunicationScene, LevelEditorScene, EndScene */ ];
+const scenes = [ MenuScene, SpaceScene, /* TextScene, CommunicationScene, LevelEditorScene, EndScene */ ];
 
 const config = {
     type: Phaser.AUTO,
