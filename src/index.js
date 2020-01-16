43b44baaf948db8b6dff8507a62551839c9ed37c
Andre LaFleur <andre.lafleur@utah.edu>
2020-01-16 10:48:14 -0700

    Move everything over to src to aid in development

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-function component() {
-	const element = document.createElement('div');
-	element.innerHTML = 'Hello';
-	return element;
-}
+import Phaser from 'phaser';
+import { Constants } from './const/index.js';
+import { MenuScene } from './scenes/menu/menuScene.js';
+// import { SpaceScene } from './scenes/space-scene/spaceScene.js';
+// import { LevelEditorScene } from './scenes/level-editor/levelEditorScene.js';
+// import { TextScene } from './scenes/text-scene/textScene.js';
+// import { CommunicationScene } from './scenes/communication-scene/communicationScene.js';
+// import { EndScene } from './scenes/end-scene/endScene.js';
 
-document.body.appendChild(component());
+const screenDimensions = Constants.dimensions.screen;
+const scenes = [ MenuScene, /* SpaceScene, TextScene, CommunicationScene, LevelEditorScene, EndScene */ ];
+
+const config = {
+    type: Phaser.AUTO,
+    width: screenDimensions.width,
+    height: screenDimensions.height,
+    parent: 'game',
+    scene: scenes,
+    physics: {
+    	default: 'arcade',
+    	arcade: {
+    		gravity: {},
+    		debug: Constants.config.debugPhysics,
+    	},
+    },
+    dom: {
+      createContainer: true,
+    },
+};
+
+const game = new Phaser.Game(config);
