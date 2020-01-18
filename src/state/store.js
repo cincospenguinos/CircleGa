9c9eb8556afa85954feb9d85bcf5099b9e09848b
Andre LaFleur <andre.lafleur@utah.edu>
2020-01-17 20:30:16 -0700

    First level is now balanced to an early game player
    - I still have some more experimentation to do, but it looks like we are in a good place to experiment now

--- src/state/store.js
+++ src/state/store.js
@@ -4,7 +4,7 @@ import reducer from './reducers/index.js';
 const player = {
 	acceleration: Math.PI / 1024,
 	maxVelocity: Math.PI / 128,
-	bulletVelocity: 15,
+	bulletVelocity: 12,
 	maxBullets: 1,
 	bulletCooldown: 30,
 };
